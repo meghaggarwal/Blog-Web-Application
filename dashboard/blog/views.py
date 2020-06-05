76871628e55c196763b998bf3f0f056a761c16f7
= <meghaa.2703@gmail.com>
2020-06-05 15:16:22 +0530

    Blog added for home page

---
+++ dashboard/blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
+# Create your views here.
+
+
+def home(request):
+  
+        context = {
+            'posts': Post.objects.all()
+        }
+        return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+def about(request):
+    context = {
+        'title':'about'
+    }
+
+    return render(request, 'blog/about.html', context)
